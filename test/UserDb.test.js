a56e40470a19a21b77b1101032e39aa9cc1e0ff4
dzdidi <deniszalessky@gmail.com>
2023-02-15 08:23:16 +0100

    Test: minor fixes in userDb test
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- test/UserDb.test.js
+++ test/UserDb.test.js
@@ -4,10 +4,9 @@ const assert = require('assert')
 const UserDb = require('../src/UserDb')
 const util = require('../src/util')
 
-let udb = null
 async function getUdb () {
-  udb = new UserDb({
-    path: './db',
+  let udb = new UserDb({
+    path: './test-db',
     name: `test-${util.rnd()}db`
   })
   await udb.init()
@@ -15,12 +14,16 @@ async function getUdb () {
 }
 
 describe('UserDb', () => {
-  it('Should create instance User Db', async () => {
+  let udb = null
+  before(async () => {
     udb = await getUdb()
   })
 
+  after(async () => {
+    await udb.db.deleteSqlite()
+  })
+
   it('Should insert new user feed info and fetch from db ', async () => {
-    udb = await getUdb()
     const uid = util.rnd()
     const fk = util.rnd()
     const ek = util.rnd()
@@ -28,19 +31,25 @@ describe('UserDb', () => {
       user_id: uid,
       feed_key: fk,
       encrypt_key: ek,
-      meta: { test: 123 }
+      meta: { test: 1 }
     })
+
+    await udb.insert({
+      user_id: util.rnd(),
+      feed_key: util.rnd(),
+      encrypt_key: util.rnd(),
+      meta: { test: 2 }
+    })
+
     const data = await udb.findByUser(uid)
-    assert(typeof data === 'object')
     assert(data.user_id === uid)
     assert(data.feed_key === fk)
     assert(data.encrypt_key === ek)
     assert(data.state === 1)
-    assert(data.meta.test === 123)
+    assert(data.meta.test === 1)
   })
 
   it('Should remove user ', async () => {
-    udb = await getUdb()
     const uid = util.rnd()
     const fk = util.rnd()
     const ek = util.rnd()
@@ -55,9 +64,5 @@ describe('UserDb', () => {
     await udb.removeUser(uid)
     data = await udb.findByUser(uid)
     assert(!data)
-  })
-
-  afterEach(async () => {
-    await udb.db.deleteSqlite()
   })
 })
